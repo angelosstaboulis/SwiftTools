e144d6dad641cce541725a46dea46cdde6d465bb
Angelos Staboulis <angelosstaboulis@gmail.com>
2024-04-24 15:57:18 +0300

    Commit refactor code

--- Package.swift
+++ Package.swift
@@ -5,36 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftTools",
-    
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftTools",
             targets: ["SwiftTools"]),
-
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftTools",
-            dependencies: []
-        ),
-        .target(
-            name: "SwiftToolsString",
-            dependencies: []
-        ),
-        .target(
-            name: "SwiftToolsMathematics",
-            dependencies: []
-        ),
-        .target(
-            name: "SwiftToolsArray",
-            dependencies: []
-        ),
+            name: "SwiftTools"),
         .testTarget(
             name: "SwiftToolsTests",
             dependencies: ["SwiftTools"]),
-            
     ]
 )
